7e0e2d5e78c54eb42c7020b9dca8ec1ea4ab2b79
Amna <amnaaldaraji@gmail.com>
2025-10-06 11:38:05 +0200

    a

--- BlazorÖvningar/obj/Debug/net9.0/BlazorÖvningar.AssemblyInfo.cs
+++ BlazorÖvningar/obj/Debug/net9.0/BlazorÖvningar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorÖvningar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab05da2bef0e221ce16a869236a87a7998019760")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorÖvningar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorÖvningar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
